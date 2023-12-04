c4313dab4f25d27133899911980a24d7a63f5aa6
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-12-04 13:21:44 +0100

    Set Filename parameters with args
    Contributes to CURA-10831

--- runner_scripts/cura_installer_filename.py
+++ runner_scripts/cura_installer_filename.py
@@ -1,12 +1,29 @@
+import argparse
 import os
 
-enterprise = "-Enterprise" if "${{ inputs.enterprise }}" == "true" else ""
-installer_filename = f"UltiMaker-Cura-{os.getenv('CURA_VERSION_FULL')}{enterprise}-linux-${{ inputs.architecture }}"
-output_env = os.environ["GITHUB_OUTPUT"]
-content = ""
-if os.path.exists(output_env):
-    with open(output_env, "r") as f:
-        content = f.read()
-with open(output_env, "w") as f:
-    f.write(content)
-    f.writelines(f"INSTALLER_FILENAME={installer_filename}\n")
+
+def set_installer_filename(args):
+    os_name = {"Linux": "linux", "Windows": "win64", "macOS": "macos"}.get(args.os)
+    enterprise = "-Enterprise" if args.enterprise == "true" else ""
+    internal = "-Internal" if args.internal == "true" else ""
+
+    installer_filename = f"UltiMaker-Cura-{os.getenv('CURA_VERSION_FULL')}{enterprise}{internal}-{os_name}-{args.architecture}"
+    output_env = os.environ["GITHUB_OUTPUT"]
+    content = ""
+    if os.path.exists(output_env):
+        with open(output_env, "r") as f:
+            content = f.read()
+
+    with open(output_env, "w") as f:
+        f.write(content)
+        f.writelines(f"INSTALLER_FILENAME={installer_filename}\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'Set the installer filename')
+    parser.add_argument('--os', type = str, help = 'OS')
+    parser.add_argument('--architecture', type = str, help = 'Architecture')
+    parser.add_argument('--enterprise', type = str, help = 'Enterprise')
+    parser.add_argument('--internal', type = str, help = 'Internal')
+    args = parser.parse_args()
+    set_installer_filename(args)
