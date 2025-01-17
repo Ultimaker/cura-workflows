fa7f7c442908bf21db075c5ccfa3fe7a3e770aed
Frederic Meeuwissen <13856291+Frederic98@users.noreply.github.com>
2025-01-17 13:38:08 +0100

    Download the right AppImageTool

--- runner_scripts/appimage_setup.sh
+++ runner_scripts/appimage_setup.sh
@@ -1,7 +1,7 @@
 # Get the AppImage tool
 sudo apt install coreutils binutils patchelf desktop-file-utils fakeroot fuse squashfs-tools strace util-linux zsync libgdk-pixbuf2.0-dev -y
 
-wget --no-check-certificate --quiet https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O $GITHUB_WORKSPACE/appimagetool
+wget --no-check-certificate --quiet "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$(uname -m).AppImage" -O $GITHUB_WORKSPACE/appimagetool
 chmod +x $GITHUB_WORKSPACE/appimagetool
 echo "APPIMAGETOOL_LOCATION=$GITHUB_WORKSPACE/appimagetool" >> $GITHUB_ENV
 
