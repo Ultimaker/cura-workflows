fd67f410fd972e3a0ad01c1e3eb3e528b4cbe88e
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2025-02-26 11:33:06 +0100

    Allow for specific user and channel

--- runner_scripts/upload_conan_recipes.py
+++ runner_scripts/upload_conan_recipes.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import re
 import yaml
+import json
 
 from pathlib import Path
 
@@ -16,22 +17,40 @@ def upload_changed_recipes(args):
         configs[package_name] = config_path
 
     packages = []
-    channel = "stable" if "main" in args.branch else re.match(r"(CURA|NP|PP)-\d*", args.branch)[0].lower().replace("-", "_")
+    is_release = "main" in args.branch
+    channel = "" if is_release else re.match(r"(CURA|NP|PP)-\d*", args.branch)[0].lower().replace("-", "_")
+    user = "" if is_release else "ultimaker"
 
     for name, config in configs.items():
         if not config.exists():
             continue
 
+        actual_user = user
+        actual_channel = channel
+
         versions = {}
         with open(config, "r") as f:
-            versions = yaml.safe_load(f)["versions"]
+            config = yaml.safe_load(f)
+            versions = config["versions"]
+            if "user" in config:
+                actual_user = config["user"]
+            if "channel" in config:
+                actual_channel = config["channel"]
 
         for version, data in versions.items():
             conanfile = config.parent.joinpath(data["folder"], "conanfile.py")
-            package = f"{name}/{version}@{args.user}/{channel}"
-            subprocess.run(["conan", "export", conanfile, "--name", name, "--version", version, "--user", args.user, "--channel", channel], check = True)
-            subprocess.run(["conan", "upload", package, "-r", args.remote, "-c"], check = True)
-            packages.append(package)
+
+            conan_export = ["conan", "export", conanfile, "--name", name, "--version", version, "-f", "json"]
+
+            if actual_user != "":
+                conan_export += ["--user", actual_user]
+            if actual_channel != "":
+                conan_export += ["--channel", actual_channel]
+
+            export_output = subprocess.run(conan_export, capture_output=True, check = True).stdout
+            package_reference = json.loads(export_output)["reference"]
+            subprocess.run(["conan", "upload", package_reference, "-r", args.remote, "-c"], check = True)
+            packages.append(package_reference.split("#")[0])
 
     summary_env = os.environ["GITHUB_STEP_SUMMARY"]
     with open(summary_env, "w") as f:
@@ -41,7 +60,6 @@ def upload_changed_recipes(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Upload all the changed recipes in the recipe folder')
-    parser.add_argument('--user', type = str, help = 'Conan package user')
     parser.add_argument('--branch', type = str, help = 'Development branch')
     parser.add_argument('--remote', type = str, help = 'Name of the remote conan repository')
     parser.add_argument("Files", metavar="FILES", type=str, nargs="+", help="Files or directories to format")
