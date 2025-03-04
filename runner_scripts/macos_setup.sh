be451deb4207aa7aa27fe9a9fea5f3b5905ff6d3
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2025-03-04 08:36:45 +0100

    Install required libtool on mac

--- runner_scripts/macos_setup.sh
+++ runner_scripts/macos_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Install required system dependencies"
-brew install autoconf automake ninja wget 2>&1 | grep -v "is already installed and up-to-date" || true
+brew install autoconf automake ninja wget libtool 2>&1 | grep -v "is already installed and up-to-date" || true
