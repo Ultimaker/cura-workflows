557349966222955ff58f76f85741bd308c3a7a58
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2026-01-06 07:46:16 +0100

    Revert "Add ability to force upload a recipe when publishing a release package"
    This reverts commit 59e58eb59968eebde4ed95240f72a7d3f34360c8.

--- runner_scripts/upload_conan_package.py
+++ runner_scripts/upload_conan_package.py
@@ -20,20 +20,13 @@ def upload_conan_package(args):
             remote = "cura-private-conan2" if "@internal" in package else "cura-conan2"
 
             print(f"Upload package {package} to {remote}")
-            process_args = ["conan", "upload", package, "-r", remote, "-c"]
-
-            if args.dry_run:
-              process_args.append("--dry-run")
-            if args.force:
-              process_args.append("--force")
-
-            subprocess.run(process_args, check=True)
+            if not args.dry_run:
+                subprocess.run(["conan", "upload", package, "-r", remote, "-c"], check=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Upload the given local package(s) to the proper Cura conan repository')
     parser.add_argument('package', type = str, help = 'Package name, fully specific or containing wildards')
     parser.add_argument('--dry-run', action='store_true', help = 'Do not upload the package but just show what would happen')
-    parser.add_argument('--force', action='store_true', help = 'Force upload the package even if it already exists (e.g. to update the timestamp)')
 
     args = parser.parse_args()
     upload_conan_package(args)
